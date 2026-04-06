40d814269a2e11ace0a0ca8e4dbedadee5198113
kimrichardj@gmail.com <kimrichardj@gmail.com>
2026-04-05 20:51:59 -0700

    start

--- obj/Debug/net10.0-android/ipdfreely.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/ipdfreely.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ipdfreely")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9bb6dccfbdb0aafa013530e345f1833dd7463461")]
 [assembly: System.Reflection.AssemblyProductAttribute("ipdfreely")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ipdfreely")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
